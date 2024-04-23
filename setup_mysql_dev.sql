f9f6af580751959036ea35c89e64e64e95a383d4
Okafor Patrick Chibuike <patrickpeters911@gmail.com>
2024-04-23 03:19:24 +0100

    set up MySQL server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
 -- script that prepares a MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'p4Christ+';
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'p4Christ+';
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
 
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev' @'localhost';
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
 
 FLUSH PRIVILEGES;
