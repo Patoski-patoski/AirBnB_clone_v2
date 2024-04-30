db04f09f58219633ec66e6558b20231a7c3bea4e
Okafor Patrick C <patrickpeters911@gmail.com>
2024-04-30 16:09:20 -0700

    a fabric script  creates and distributes an archive to your web servers

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+""" 3-deploy_web_static"""
+
+
+from fabric.api import local, env, put, run
+from datetime import datetime
+from os.path import exists
+
+
+env.hosts = ["35.175.128.192", "54.88.205.156"]
+
+
+def do_pack():
+    """ do_pack: Generates a .tgz archive from the contents of the web_static
+
+        Args: None
+
+        Return:  the archive path if the archive has been correctly generated.
+        otherwise, None
+    """
+
+    get_time = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Creates the versions directory
+    local("mkdir -p versions")
+
+    # Create the archived file
+    tgz_file = f"versions/web_static_{get_time}.tgz"
+
+    # archiving process
+    archived = local(f"tar -cvzf {tgz_file} web_static")
+
+    if not archived.failed:
+        return archived
+    return None
+
+
+def do_deploy(archive_path):
+    """ do_deploy: distributes an archive to your web servers
+        Args:
+            archive_path(str): param 1
+
+        Return True if args, False otherwise
+    """
+
+    if exists(archive_path) is False:
+        return False
+    try:
+        put(archive_path, "/tmp/")
+
+        first_split = archive_path.split('/')[-1]
+        second_split = first_split.split('.')[0]
+
+        path = f"/data/web_static/releases/{second_split}"
+        run(f"mkdir -p {path}/")
+        local(f"mkdir -p {path}/")
+        run(f"tar -xzf /tmp/{first_split} -C {path}")
+        # Delete the archive from the web server
+        run(f"rm -fr /tmp/{first_split}")
+        run(f"rm -rf {path}/web_static")
+        local(f"rm -rf {path}/web_static")
+        # Delete the symbolic link /data/web_static/current from the web server
+        run(f"rm -rf /data/web_static/current")
+        local(f"rm -rf /data/web_static/current")
+        run(f"ln -sf {path}/ /data/web_static/current")
+        local(f"ln -sf /AirBnB_clone_v2/web_static /data/web_static/current")
+
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
+
+
+def deploy():
+    """ deploy: a fabric script that creates and distributes an archive to your
+        web servers
+    """
+    #run("Executing task \'deploy\'")
+    archived = do_pack()
+    my_deploy = do_deploy(archived)
+
+    return my_deploy
+
+
+
+
