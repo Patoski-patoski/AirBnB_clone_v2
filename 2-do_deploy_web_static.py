00dc13976b2417a90a34abf923fd4caa83f136ad
Okafor Patrick C <patrickpeters911@gmail.com>
2024-04-29 23:56:36 -0700

    a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""  1-pack_web_static.py: a Fabric script that generates a .tgz archive from
+the contents of the web_static folder of your AirBnB Clone repo
+"""
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ do_pack: Generates a .tgz archive from the contents of the web_static
+
+        Args: None
+
+        Return:  the archive path if the archive has been correctly generated.
+        otherwise, None
+    """
+
+    get_time = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Creates the versions directory
+    local("mkdir -p versions")
+
+    # Create the archived file
+    tgz_file = f"versions/web_static_{get_time}.tgz"
+
+    # archiving process
+    archived = local(f"tar -cvzf {tgz_file} web_static")
+
+    if not archived.failed:
+        return archived
+    return None
+
+
+
+def do_deploy(archive_path):
+    """distributes an archive to your web servers
+    """
+    if exists(archive_path) is False:
+        return False
+    try:
+        put(archive_path, "/tmp/")
+        file_name = archive_path.split("/")[-1]
+        path = "/data/web_static/releases/{}".format(file_name.split('.')[0])
+        run("mkdir -p {}".format(path))
+        run("tar -xzf /tmp/{} -C {}/".format(file_name, path))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}/web_static/* {}/".format(path, path))
+        run("rm -rf {}/web_static".format(path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(path))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
