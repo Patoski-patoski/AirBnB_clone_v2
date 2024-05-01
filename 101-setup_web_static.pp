31078fd8e4648f5fa828dc96d45297b1935745f5
Okafor Patrick C <patrickpeters911@gmail.com>
2024-04-30 22:49:55 -0700

    puppet setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -30,6 +30,72 @@ file { ['/data/web_static/releases/test/', '/data/web_static/shared/']:
   mode   => '0755',
 }
 
+class web_server_setup {
+  # Ensure Nginx is installed
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  # Create the /data/web_static/releases/test directory
+  file { '/data/web_static/releases/test':
+    ensure  => directory,
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    mode    => '0755',
+  }
+
+  # Create the /data/web_static/shared directory
+  file { '/data/web_static/shared':
+    ensure  => directory,
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    mode    => '0755',
+  }
+
+  # Create a fake HTML file /data/web_static/releases/test/index.html
+  file { '/data/web_static/releases/test/index.html':
+    ensure  => file,
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    mode    => '0644',
+    content => '<html><head></head><body><h1>Adam is almost a Full Stack Software Engineer</h1></body></html>',
+  }
+
+  # Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
+  file { '/data/web_static/current':
+    ensure  => symlink,
+    target  => '/data/web_static/releases/test/',
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+  }
+
+  # Give ownership of the /data/ folder to the ubuntu user AND group
+  file { '/data':
+    ensure  => directory,
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    recurse => true,
+  }
+
+  # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    notify  => Service['nginx'],
+    require => Package['nginx'],
+    content => template('web_server_setup/nginx_config.erb'),
+  }
+
+  # Ensure Nginx service is running
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
+
+
 # Create the index.html file
 file { '/data/web_static/releases/test/index.html':
   ensure  => file,
