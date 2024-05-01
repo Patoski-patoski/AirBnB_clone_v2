9789e23d3f422f0a0681ff7525f33b460dfffed7
Okafor Patrick C <patrickpeters911@gmail.com>
2024-04-30 22:23:09 -0700

    puppet setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,66 +1,68 @@
-# A manifest that sets up your web servers for the deployment of web_static
-
-class web_server_setup {
-  # ensure nginx is installed
-  package { 'nginx':
-    ensure => installed,
+# Install Nginx if not installed
+package { 'nginx':
+  ensure => installed,
 }
 
-# Create the /data/web_static/releases/test directory
-file { '/data/web_static/releases/test':
+# Restart Nginx
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  restart => true,
+}
+
+# Create necessary directories
+file { ['/data/web_static/releases/test/', '/data/web_static/shared/']:
   ensure => directory,
   owner  => 'ubuntu',
   group  => 'ubuntu',
-  mode   => '0755',
 }
 
-# Create the /data/web_static/shared directory
-file { '/data/web_static/shared':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0755',
-}
-
-# Create a fake HTML file /data/web_static/releases/test/index.html
+# Create the index.html file
 file { '/data/web_static/releases/test/index.html':
   ensure  => file,
+  content => '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>',
   owner   => 'ubuntu',
   group   => 'ubuntu',
-  mode    => '0644',
-  content => '<html><head></head><body>Holberton School</body></html>'
 }
 
-# Create a symbolic link /data/web_static/current linked to the
-# /data/web_static/releases/test/ folder
+# Create and remove the existing symbolic link if it exists
 file { '/data/web_static/current':
-  ensure => symlink,
+  ensure => 'link',
   target => '/data/web_static/releases/test/',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+}
+
+# Create the nginx configuration
+file { '/etc/nginx/sites-available/hbnb.conf':
+  ensure => file,
+  content => 'server {
+        listen 80;
+        listen [::]:80;
+        server_name www.patoski.tech patoski.tech;
+        add_header X-Served-By $HOSTNAME;
+        location /hbnb_static/ {
+                alias /data/web_static/current/;
+                autoindex off;
+        }
+        index index.html;
+}',
+}
+
+# Create a symbolic link to make te site like
+file { '/etc/nginx/sites-enabled/hbnb.conf':
+  ensure => symlink,
+  target => '/etc/nginx/sites-available/hbnb.conf',
 }
 
 # Give ownership of the /data/ folder to the ubuntu user AND group
-file { '/data':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+file { '/data/':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
   recurse => true,
 }
-
-# Update the Nginx configuration to serve the content of
-# /data/web_static/current/ to hbnb_static
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  notify  => Service['nginx'],
-  require => Package['nginx'],
-  content => template('web_server_setup/nginx_config.erb'),
-}
-
-# Ensure Nginx service is running
-service { 'nginx':
-  ensure => running,
