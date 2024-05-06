de218464fbc524726e98b416b06ec90b660c3d03
Okafor Patrick C <patrickpeters911@gmail.com>
2024-05-06 16:11:11 +0000

    a script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello():
     """ display 'Hello HBNB'  """
-    return "Hello HNBN!"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
