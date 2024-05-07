78b1c3709d1b278cdea95fc79a7a2c13a7b5ab65
Okafor Patrick C <patrickpeters911@gmail.com>
2024-05-07 07:27:08 +0000

    displays Python followed by the value of the text variable

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" 3-python_route """
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """ index: a function that displays "Hello HBNB!" """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ hbnb: a function that displays "HBNB" """
+    return "HBNB"
+
+
+def replace(strings):
+    """ replace: replace strings containing underscore(_) with space (" ")
+    Args:
+         (str): Strings to be replaced
+    Return:
+         New and replaced string!
+    """
+    new_str = ""
+    for s in strings:
+        if s == '_':
+            s = ' '
+        new_str += (s)
+    return new_str
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """ c_is_fun: display "C" followed by the value of the text variable """
+    replaced_text = escape(text)
+    return "C {}".format(replace(replaced_text))
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def run_python(text):
+    """ python: display "Python", followed by the value of text variable """
+    replaced_text = escape(text)
+    return "Python {}".format(replace(replaced_text))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
