aee37565eb5a4ae122189532468910b09d3f6c4f
Okafor Patrick C <patrickpeters911@gmail.com>
2024-05-07 03:49:36 +0000

    a script that starts a Flask web application: routes /hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,15 +6,18 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def index():
     """index: a function that displays  "Hello HBNB!" """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def display():
     """display: a function that displays  "HBNB" """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
