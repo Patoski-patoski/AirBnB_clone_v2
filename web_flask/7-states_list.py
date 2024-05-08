7bf82b06d971855ef3ffe85031705273e34c0da4
Okafor Patrick C <patrickpeters911@gmail.com>
2024-05-08 17:27:14 +0000

    list of all State objects present in DBStorage sorted by name (A->Z)

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" 7-states_list.py """
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ teardown: to remove current sqlalchemy Session """
+    if storage.__session is not None:
+        storage.close()
+
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """A method to render an HTML page with a list of all State objects
+    """
+    # test start
+    storage.reload()
+    # ends
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
